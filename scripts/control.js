c5865858b2f3639bc6f8b256ee0af07dca2f42fb
RMS <reishmax@gmail.com>
2024-09-25 13:09:39 +0500

    added share feature

--- scripts/control.js
+++ scripts/control.js
@@ -3,6 +3,7 @@ import { instructionsStart } from './instructions.js'
 const controls = document.querySelector('.control')
 
 const controlHelp = document.getElementById('control-help')
+const controlShare = document.getElementById('control-share')
 
 export const isControlElement = (element) => {
   return Boolean(element.closest('.control'))
@@ -12,7 +13,23 @@ export const toggleControlShow = () => {
   controls.classList.toggle('control-hidden')
 }
 
+const hideControlItem = (element) => {
+  element.classList.add('control-item-none')
+}
+
+// show help
 controlHelp.addEventListener('click', () => {
   toggleControlShow()
   instructionsStart()
+})
+
+// check whether this device has navigator.share
+// and disable share control if it does not
+if (!navigator.share) {
+  hideControlItem(controlShare)
+}
+
+controlShare.addEventListener('click', () => {
+  const text = `Turn on the color! It's one click away: ${window.location.href}`
+  navigator.share({ url: window.location.href, text })
 })
